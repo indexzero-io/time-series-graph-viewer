78677e08bfff4df5605331e08c40de0b51802331
indexzero-hp1 <andy.quijano@indexzero.io>
2026-07-24 10:38:59 -0600

    update avatars.

--- data.js
+++ data.js
@@ -1,19 +1,19 @@
 const chartDataConfig = {
   "participants": {
-    "Andy Quijano": { "imageUrl": "https://i.pravatar.cc/150?u=Andy+Quijano", "color": "#33FF8F", "initialValue": 0 },
-    "Vanesa Castillo": { "imageUrl": "https://i.pravatar.cc/150?u=Vanesa+Castillo", "color": "#33FF57", "initialValue": 0 },
-    "Hugo Palomares": { "imageUrl": "https://i.pravatar.cc/150?u=Hugo+Palomares", "color": "#FF5733", "initialValue": 0 },
-    "Paulina Rodríguez": { "imageUrl": "https://i.pravatar.cc/150?u=Paulina+Rodriguez", "color": "#338FFF", "initialValue": 0 },
-    "joaco": { "imageUrl": "https://i.pravatar.cc/150?u=joaco", "color": "#33FFF5", "initialValue": 0 },
-    "Dana Tellez": { "imageUrl": "https://i.pravatar.cc/150?u=Dana+Tellez", "color": "#FF338F", "initialValue": 0 },
-    "René Aldrette": { "imageUrl": "https://i.pravatar.cc/150?u=Rene+Aldrette", "color": "#F5FF33", "initialValue": 0 },
-    "Oscar Cortes": { "imageUrl": "https://i.pravatar.cc/150?u=Oscar+Cortes", "color": "#A833FF", "initialValue": 0 },
-    "Jose Carvajal": { "imageUrl": "https://i.pravatar.cc/150?u=Jose+Carvajal", "color": "#3357FF", "initialValue": 0 },
-    "Cesar": { "imageUrl": "https://i.pravatar.cc/150?u=Cesar", "color": "#FF3333", "initialValue": 0 },
-    "Cristina Bobadilla": { "imageUrl": "https://i.pravatar.cc/150?u=Cristina+Bobadilla", "color": "#FF33A8", "initialValue": 0 },
-    "Vanessa Welsh": { "imageUrl": "https://i.pravatar.cc/150?u=Vanessa+Welsh", "color": "#8F33FF", "initialValue": 0 },
-    "Majo cano garcia": { "imageUrl": "https://i.pravatar.cc/150?u=Majo+cano+garcia", "color": "#FF8F33", "initialValue": 0 },
-    "Luis Mariano Montemayor": { "imageUrl": "https://i.pravatar.cc/150?u=Luis+Mariano+Montemayor", "color": "#8FFF33", "initialValue": 0 }
+    "Andy Quijano": { "imageUrl": "https://ca.slack-edge.com/T0AL49HDW-U0AL0BD9A-daa3a4c37479-512", "color": "#33FF8F", "initialValue": 0 },
+    "Vanesa Castillo": { "imageUrl": "https://media-qro1-1.cdn.whatsapp.net/v/t61.24694-24/491839157_1912470829568233_1695296547667844652_n.jpg?ccb=11-4&oh=01_Q5Aa5AEOZ7LZiDZ1dADBDhAdzRzIBmn7Gs4UYjuWxZrh-bWLQA&oe=6A70BEDC&_nc_sid=5e03e0&_nc_cat=103", "color": "#33FF57", "initialValue": 0 },
+    "Hugo Palomares": { "imageUrl": "https://cdn.discordapp.com/avatars/1390014915049291787/32c07ac5458da17dcd23356b77c91f7c.webp", "color": "#FF5733", "initialValue": 0 },
+    "Paulina Rodríguez": { "imageUrl": "https://media-qro1-1.cdn.whatsapp.net/v/t61.24694-24/629254755_1891726834830235_551839730620973806_n.jpg?ccb=11-4&oh=01_Q5Aa5AHpKI-eU7o2QJkyChSbRAgg65QNWRtV_M1f0sSlaJUL9g&oe=6A70A9D4&_nc_sid=5e03e0&_nc_cat=110", "color": "#338FFF", "initialValue": 0 },
+    "joaco": { "imageUrl": "https://ca.slack-edge.com/T0AL49HDW-U41KAMV0F-ge3a68575589-192", "color": "#33FFF5", "initialValue": 0 },
+    "Dana Tellez": { "imageUrl": "https://media-qro1-1.cdn.whatsapp.net/v/t61.24694-24/636850713_26491530877141448_4409842106719811040_n.jpg?ccb=11-4&oh=01_Q5Aa5AFSEm513JOJHpkiLffvcBbV1IUTs03JPC9oL2jkLxjKsg&oe=6A709921&_nc_sid=5e03e0&_nc_cat=110", "color": "#FF338F", "initialValue": 0 },
+    "René Aldrette": { "imageUrl": "https://cdn.discordapp.com/avatars/749839296256540692/bb614a6b2b18fe4bdee4079cf24340f3.webp", "color": "#F5FF33", "initialValue": 0 },
+    "Oscar Cortes": { "imageUrl": "https://cdn.discordapp.com/avatars/1475511633714610410/c2c0debfadc701bb64aac3b553977a45.webp", "color": "#A833FF", "initialValue": 0 },
+    "Jose Carvajal": { "imageUrl": "https://ca.slack-edge.com/T0AL49HDW-UBP182QBU-8d8ce7882d27-512", "color": "#3357FF", "initialValue": 0 },
+    "Cesar": { "imageUrl": "https://ca.slack-edge.com/T0AL49HDW-UJXDFGG3C-47b7818066ac-512", "color": "#FF3333", "initialValue": 0 },
+    "Cristina Bobadilla": { "imageUrl": "https://media-qro1-1.cdn.whatsapp.net/v/t61.24694-24/620170374_1393469455604262_958575360644994708_n.jpg?ccb=11-4&oh=01_Q5Aa5AGujifH-2f5Xv4g30j_PkZdbK2F4VWMy4lb-vZK62k8LQ&oe=6A70B5F4&_nc_sid=5e03e0&_nc_cat=102", "color": "#FF33A8", "initialValue": 0 },
+    "Vanessa Welsh": { "imageUrl": "https://media-qro1-1.cdn.whatsapp.net/v/t61.24694-24/460943091_487180967706521_4223315002341919291_n.jpg?ccb=11-4&oh=01_Q5Aa5AHCdDNOVQoMkHAAYbGjf9uJK5vdRY9jOBy8ynYrH4M2nA&oe=6A709E18&_nc_sid=5e03e0&_nc_cat=108", "color": "#8F33FF", "initialValue": 0 },
+    "Majo cano garcia": { "imageUrl": "https://ca.slack-edge.com/T0AL49HDW-U04NE43TGLC-70ea670f2464-192", "color": "#FF8F33", "initialValue": 0 },
+    "Luis Mariano Montemayor": { "imageUrl": "https://ca.slack-edge.com/T0AL49HDW-U6LMSF7L4-042fa53b08b9-512", "color": "#8FFF33", "initialValue": 0 }
   },
   "timelineData": [
     {
